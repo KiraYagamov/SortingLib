8e01da37066a83e1ef9432793f80d3a08e634110
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-05-07 22:19:59 +0300

    add consts

--- consts.h
+++ consts.h
@@ -1,5 +1,17 @@
 // #define SORT_DIRECTION 1
 // #define SORT_METHOD 1
 
-int SORT_DIRECTION = 1;
-int SORT_METHOD = 1;
+#define INCREASE 1
+#define DECREASE -1
+
+#define BUBBLE 1
+#define INSERTION 2
+#define SELECTION 3
+#define COMB 4
+#define SHELL 5
+#define QUICK 6
+#define MERGE 7
+#define HEAP 8
+
+int SORT_DIRECTION = INCREASE;
+int SORT_METHOD = BUBBLE;
