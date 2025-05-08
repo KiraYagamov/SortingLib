84840705023b46114f7238e25a435212f595bce9
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-05-08 15:31:32 +0300

    mac fix

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "lib/sorting_lib.h"
+#include "lib/tests.h"
+
+int main() {
+    // int arr[] = {5, 7, 3, 4, 8, 0, 2, 1, 6};
+    // // char arr[] = {'b', 'g', 'v', 'n', 'm', 'y', 'a', 'c', 'd', 'e'};
+    // int length = sizeof arr / sizeof arr[0];
+    // sort(arr, length, sizeof arr[0], compInt);
+    // print_int_arr(arr, length);
+    test_int(30000);
+    return 0;
+}
