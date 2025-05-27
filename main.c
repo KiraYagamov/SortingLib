270a144880f02843b8f4356f9200f189934b265c
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-05-27 09:50:55 +0300

    update

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "lib/tests.h"
 
 int main() {
-    // int arr[] = {5, 7, 3, 4, 8, 0, 2, 1, 6};
-    // // char arr[] = {'b', 'g', 'v', 'n', 'm', 'y', 'a', 'c', 'd', 'e'};
+    // // int arr[] = {5, 7, 3, 4, 8, 0, 2, 1, 6};
+    // char arr[] = {'b', 'g', 'v', 'n', 'm', 'y', 'a', 'c', 'd', 'e'};
     // int length = sizeof arr / sizeof arr[0];
-    // sort(arr, length, sizeof arr[0], compInt);
-    // print_int_arr(arr, length);
-    test_double(10000);
+    // sort(arr, length, sizeof arr[0], compChar);
+    // print_char_arr(arr, length);
+    test_char(10000);
     return 0;
 }
